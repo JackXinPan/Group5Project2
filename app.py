cd16a3dfabf68aad75175c307059ba9e7d6b1ae8
JackXinPan <79770660+JackXinPan@users.noreply.github.com>
2021-07-15 20:12:47 +0800

    flask and heroku

---
+++ app.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///plankton.sqlite")
+conn = engine.connect()
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+#Passenger = Base.classes.passenger
+
+
+# Save references to each table
+# Phytoplankton = Base.classes.phytoplankton
+# Zooplankton = Base.classes.zooplankton
+# PhytoplanktonColor = Base.classes.phytoplankton_color_index
+
+
+
+# Query plankton Records in the the Database
+phyto_data = pd.read_sql("SELECT * FROM phytoplankton", conn)
+phyto_data.to_dict()
+phyto__color_index_data = pd.read_sql("SELECT * FROM phytoplankton_color_index", conn)
+phyto__color_index_data.to_dict()
+zoo_data = pd.read_sql("SELECT * FROM zooplankton", conn)
+zoo_data.to_dict()
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+phyto_data.to_dict()
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    print("Server received request for 'Home' page...")
+    return (
+        f"Available directories<p>"
+        f"/phytoplankton<br/>"
+        f"/phytoplankton_color_index<br/>"
+        f"/zooplankton"
+    )
+
+@app.route("/phytoplankton")  
+def phyto():
+    return (phyto_data.to_dict())
+
+@app.route("/phytoplankton_color_index")  
+def phyto_color():
+    return (phyto__color_index_data.to_dict())
+
+@app.route("/zooplankton")  
+def zoo():
+    return (zoo_data.to_dict())
+
+
+# #phyto data
+# @app.route("/phytoplankton")
+# def phytoplankton():
+#     return jsonify(Phytoplankton)
+
+
+# open server
+if __name__ == '__main__':
+    app.run(debug=True)
