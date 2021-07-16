e9dbc46558071df2c14eb37204a557207e8d4cf9
JackXinPan <79770660+JackXinPan@users.noreply.github.com>
2021-07-17 07:04:34 +0800

    where2010

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ Base.prepare(engine, reflect=True)
 
 
 # Query plankton Records in the the Database
-phyto_data = pd.read_sql("SELECT * FROM phytoplankton" , conn)
+phyto_data = pd.read_sql("SELECT * FROM phytoplankton WHERE year > 2010"  , conn)
 # phyto__color_index_data = pd.read_sql("SELECT * FROM phytoplankton_color_index WHERE year > 2010", conn)
-zoo_data = pd.read_sql("SELECT * FROM zooplankton", conn)
+zoo_data = pd.read_sql("SELECT * FROM zooplankton WHERE year > 2010" , conn)
 #################################################
 # Flask Setup
 #################################################
