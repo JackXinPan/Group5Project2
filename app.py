d6b83c46d31719a81fd4dcdb12e1f79da25e786e
JackXinPan <79770660+JackXinPan@users.noreply.github.com>
2021-07-17 04:44:19 +0800

    cut id

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///planktontest2.sqlite")
+engine = create_engine("sqlite:///planktontest3.sqlite")
 conn = engine.connect()
 # reflect an existing database into a new model
 Base = automap_base()
