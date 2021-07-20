f6ae2cec319365c79b39921b7b5d5c7b91558ea4
nk <knarisara27@gmail.com>
2021-07-20 21:47:19 +0800

    move data

---
+++ narisara/final_zoo_routetgroup.js
@@ -0,0 +1,62 @@
+[
+    {
+        "Route": "ANHO",
+        "taxon_group": "COPEPOD",
+        "taxon_per_m3": "91226.40999999856"
+    },
+    {
+        "Route": "ANHO",
+        "taxon_group": "NOCTILUCA",
+        "taxon_per_m3": "460.65999999999997"
+    },
+    {
+        "Route": "ANHO",
+        "taxon_group": "NON-COPEPOD",
+        "taxon_per_m3": "8243.65000000007"
+    },
+    {
+        "Route": "BRSY",
+        "taxon_group": "COPEPOD",
+        "taxon_per_m3": "33764.519999998105"
+    },
+    {
+        "Route": "BRSY",
+        "taxon_group": "NOCTILUCA",
+        "taxon_per_m3": "33.33"
+    },
+    {
+        "Route": "BRSY",
+        "taxon_group": "NON-COPEPOD",
+        "taxon_per_m3": "22798.129999999954"
+    },
+    {
+        "Route": "HOAN",
+        "taxon_group": "COPEPOD",
+        "taxon_per_m3": "79303.30999999806"
+    },
+    {
+        "Route": "HOAN",
+        "taxon_group": "NOCTILUCA",
+        "taxon_per_m3": "2635.3600000000006"
+    },
+    {
+        "Route": "HOAN",
+        "taxon_group": "NON-COPEPOD",
+        "taxon_per_m3": "11732.91000000009"
+    },
+    {
+        "Route": "MEAD",
+        "taxon_group": "COPEPOD",
+        "taxon_per_m3": "38887.05999999823"
+    },
+    {
+        "Route": "MEAD",
+        "taxon_group": "NOCTILUCA",
+        "taxon_per_m3": "53.35"
+    },
+    {
+        "Route": "MEAD",
+        "taxon_group": "NON-COPEPOD",
+        "taxon_per_m3": "44394.83999999944"
+    }
+]
---
+++ narisara/final_zoo_routeyear.js
@@ -0,0 +1,192 @@
+[
+    {
+        "Route": "ANHO",
+        "Year": "2007",
+        "taxon_per_m3": "41370.309999999685"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2008",
+        "taxon_per_m3": "13782.040000000012"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2009",
+        "taxon_per_m3": "8762.110000000015"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2010",
+        "taxon_per_m3": "12137.54000000003"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2011",
+        "taxon_per_m3": "5901.320000000012"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2013",
+        "taxon_per_m3": "11532.770000000024"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2016",
+        "taxon_per_m3": "1492.3400000000022"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2017",
+        "taxon_per_m3": "1616.8200000000004"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2018",
+        "taxon_per_m3": "2636.7700000000027"
+    },
+    {
+        "Route": "ANHO",
+        "Year": "2019",
+        "taxon_per_m3": "698.6999999999999"
+    },
+    {
+        "Route": "BRSY",
+        "Year": "2009",
+        "taxon_per_m3": "5696.640000000039"
+    },
+    {
+        "Route": "BRSY",
+        "Year": "2010",
+        "taxon_per_m3": "8253.820000000065"
+    },
+    {
+        "Route": "BRSY",
+        "Year": "2011",
+        "taxon_per_m3": "2805.3100000000145"
+    },
+    {
+        "Route": "BRSY",
+        "Year": "2012",
+        "taxon_per_m3": "5270.450000000028"
+    },
+    {
+        "Route": "BRSY",
+        "Year": "2014",
+        "taxon_per_m3": "22986.739999999852"
+    },
+    {
+        "Route": "BRSY",
+        "Year": "2015",
+        "taxon_per_m3": "9392.100000000013"
+    },
+    {
+        "Route": "BRSY",
+        "Year": "2016",
+        "taxon_per_m3": "2190.92"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2007",
+        "taxon_per_m3": "13340.570000000007"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2008",
+        "taxon_per_m3": "18968.47999999991"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2009",
+        "taxon_per_m3": "15492.41000000002"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2010",
+        "taxon_per_m3": "7517.570000000029"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2011",
+        "taxon_per_m3": "13518.940000000044"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2014",
+        "taxon_per_m3": "6090.3800000000265"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2015",
+        "taxon_per_m3": "3355.4700000000103"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2016",
+        "taxon_per_m3": "3092.450000000007"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2017",
+        "taxon_per_m3": "3166.480000000007"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2018",
+        "taxon_per_m3": "5075.110000000008"
+    },
+    {
+        "Route": "HOAN",
+        "Year": "2019",
+        "taxon_per_m3": "4053.7200000000043"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2010",
+        "taxon_per_m3": "2450.930000000001"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2011",
+        "taxon_per_m3": "4291.060000000017"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2012",
+        "taxon_per_m3": "8619.190000000024"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2013",
+        "taxon_per_m3": "2626.4600000000078"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2014",
+        "taxon_per_m3": "35462.97999999934"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2015",
+        "taxon_per_m3": "17764.959999999955"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2016",
+        "taxon_per_m3": "1404.5900000000015"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2017",
+        "taxon_per_m3": "5236.53000000001"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2018",
+        "taxon_per_m3": "2484.150000000015"
+    },
+    {
+        "Route": "MEAD",
+        "Year": "2020",
+        "taxon_per_m3": "2994.400000000009"
+    }
+]
